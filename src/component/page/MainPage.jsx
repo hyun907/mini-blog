94475115cb53c8bf115690cb111a14b7b2b328b8
hyun907 <seunghyun020907@naver.com>
2024-08-25 21:48:11 +0900

    feat: 검색 기능 구현

--- src/component/page/MainPage.jsx
+++ src/component/page/MainPage.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import PostList from "../list/PostList";
 import Button from "../ui/Button";
+import TextInput from "../ui/TextInput";
 import data from "../../data.json";
-
-// 처음 접속했을 때 보게 될 메인 페이지
-// 글을 작성할 수 있는 버튼, 글 목록을 보여줘야 됨
 
 const Wrapper = styled.div`
   padding: 16px;
@@ -21,17 +19,37 @@ const Container = styled.div`
   width: 100%;
   max-width: 720px;
 
-  :not(:last-child) {
+  & > * {
     margin-bottom: 16px;
   }
 `;
 
+const SearchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function MainPage(props) {
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+
+  // 검색어에 따라 posts 필터링
+  const filteredPosts = data.filter((post) =>
+    post.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <Wrapper>
       <Container>
+        <SearchWrapper>
+          <TextInput
+            height={20}
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            placeholder="검색어를 입력하세요..."
+          />
+        </SearchWrapper>
         <Button
           title="글 작성하기"
           onClick={() => {
@@ -39,7 +57,7 @@ function MainPage(props) {
           }}
         />
         <PostList
-          posts={data}
+          posts={filteredPosts}
           onClickItem={(item) => {
             navigate(`/post/${item.id}`);
           }}
